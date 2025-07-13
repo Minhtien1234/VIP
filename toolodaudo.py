ed340710b1da4e241abe199c5e018addfc76a326
Minhtien1234 <91588319+Minhtien1234@users.noreply.github.com>
2025-07-13 16:00:39 +0700

    Create toolodaudo.py

---
+++ toolodaudo.py
@@ -0,0 +1,351 @@
+from time import sleep
+import sys
+from colorama import Fore, Back, Style
+import random
+import random
+import sys
+import os
+import sys
+import time
+import json
+import os
+import sys
+import time
+import random
+import requests
+import subprocess
+import uuid
+import hashlib
+from collections import defaultdict    
+from datetime import datetime, timedelta, timezone 
+from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
+from rich import box
+from colorama import init
+from pystyle import Colors, Colorate   
+from rich.columns import Columns
+from rich.segment import Segment
+from rich.measure import Measurement
+import random
+import string
+import requests
+import base64
+import subprocess
+import uuid
+import hashlib
+from collections import defaultdict    
+from datetime import datetime, timedelta
+from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
+from rich import box
+from colorama import init
+from pystyle import Colors, Colorate
+from time import sleep
+from colorama import Fore, Back, Style
+import random
+import sys
+import os
+from rich.console import Console
+from rich.panel import Panel
+from rich import print as rprint
+from rich.prompt import Prompt
+from time import sleep
+from colorama import Fore, Back, Style
+import random
+import sys
+from time import sleep
+from colorama import Fore, Back, Style
+import requests
+import json
+from colorama import Fore, Style
+from datetime import datetime, timedelta
+from pytz import timezone
+import requests
+import time
+import json
+import sys
+import random
+import string
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+import threading
+from threading import BoundedSemaphore
+import threading
+import base64
+import os
+import time
+import re
+import json
+import random
+import requests
+import socket
+import sys
+from time import sleep
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+from time import sleep
+from datetime import datetime, timedelta
+import os
+try:
+    from faker import Faker
+    from Crypto.Cipher import AES
+    from Crypto.Util.Padding import pad
+    import requests
+    from rich.table import Table
+    from rich.panel import Panel
+    from rich import box
+    from colorama import init, Fore
+    from rich.console import Console
+except ImportError:
+    os.system('pip install Faker')
+    os.system('pip install requests')
+    os.system('pip install pycryptodome')
+    
+    
+# import lại sau khi cài đặt
+from faker import Faker
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+import requests
+
+trang = "\033[1;37m\033[1m"
+xanh_la = "\033[1;32m\033[1m"
+xanh_duong = "\033[1;34m\033[1m"
+xanhnhat = '\033[1m\033[38;5;51m'
+do = "\033[1;31m\033[1m\033[1m"
+red='\u001b[31;1m'
+yellow='\u001b[33;1m'
+green='\u001b[32;1m'
+blue='\u001b[34;1m'
+tim='\033[1;35m'
+xanhlam='\033[1;36m'
+RESET = "\033[0m"
+BOLD = "\033[1m"
+CYAN = "\033[96m"
+MAGENTA = "\033[95m"
+YELLOW = "\033[93m"
+GREEN = "\033[92m"
+BLUE = "\033[94m"
+AQUA = "\033[96m"
+LIME = "\033[92m"
+xam='\033[1;30m'
+black='\033[1;19m'
+xam = '\033[1;30m\033[1m'
+vang = "\033[1;33m\033[1m"
+tim = "\033[1;35m\033[1m"
+hongnhat = "#FFC0CB"
+kt_code = "</>"
+dac_biet = "\033[32;5;245m\033[1m\033[38;5;39m"
+
+def thanhngang(so):
+    for i in range(so):
+        print(trang+'\033[1;31m-',end ='')
+    print('')
+    
+os.system('cls' if os.name == 'nt' else 'clear')
+print(">> Loading...")
+sleep(1)
+os.system('cls' if os.name == 'nt' else 'clear')
+def xoss(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.005)
+xoss("\n\033[1;32mVui Lòng Chờ... ")
+sleep(1.5)
+os.system('cls' if os.name == 'nt' else 'clear')
+for i in range(1, 101):
+  sys.stdout.write(f"\r{BOLD}{LIME}ĐANG LOAD TOOL + GIT +: [{i}% {'█' * (i // 2)}]{RESET}")
+  sys.stdout.flush()
+  sleep(0.03)  # Điều chỉnh thời gian chờ nếu cần
+sleep(1)
+import os
+os.system("cls" if os.name == "nt" else "clear")
+def xoss(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.005)
+xoss('\n\033[1;32m[●] \033[93mĐang \033[96mLoad \033[95mAPI \033[94mTool...');time.sleep(0.10)
+xoss('\n\033[1;36m[●] \033[94mkiểm \033[93mtra \033[95msever \033[96mtool...')
+xoss('\n\033[1;33m[●] \033[96mkiểm \033[95mtra \033[94mbản \033[93mupdate.. ')
+xoss('\n\033[1;34m[●] \033[91mTiến \033[96mhành \033[94mvào \033[95mtool...')
+def Update():
+    exit('\033[1;31m[●] Đang Tiến Hành Vào Tool...... ')
+
+sleep(1)
+colors = [
+    "\033[1;37m\033[1m",  # Trắng
+    "\033[1;32m\033[1m",  # Xanh lá
+    "\033[1;34m\033[1m",  # Xanh dương 
+    "\033[1m\033[38;5;51m",  # Xanh nhạt
+    "\033[1;31m\033[1m\033[1m",  # Đỏ
+    "\033[1;30m\033{1m",  # Xám
+    "\033[1;33m\033[1m",  # Vàng
+    "\033[1;35m\033[1m",  # Tím
+    "\033[32;5;245m\033[1m\033[38;5;39m",  # Màu đặc biệt
+]
+
+os.system('cls' if os.name == 'nt' else 'clear')
+banner = f"""
+\033[1;37m\033[1m██████╗ ██╗  ██╗██╗   ██╗ ██████╗  ██████╗                                            
+\033[32;5;245m\033[1m\033[38;5;39m██████╔╝███████║██║   ██║██║   ██║██║                         
+\033[1;32m\033[1m██╔═══╝ ██╔══██║██║   ██║██║   ██║██║                           
+\033[1;34m\033[1m██║     ██║  ██║╚██████╔╝╚██████╔╝╚██████╗                      
+\033[1;30m\033[1m╚═╝     ╚═╝  ╚═╝ ╚═════╝  ╚═════╝  ╚═════╝ 
+\033[1;36m╠═══════════════════════════════════════════════╣
+\033[1;36m║\033[38;5;204m▶ \033[38;5;204mNhóm   : \033[38;5;20mhttps://zalo.me/g/mprgxe166         \033[1;36m║
+\033[1;36m║\033[38;5;204m▶ \033[38;5;204mFaceBook : \033[38;5;204mfacebook.com/phuocan.9999         \033[1;36m║
+\033[1;36m║\033[38;5;204m▶ \033[38;5;204mYoutube : \033[38;5;204myoutube.com/@phuocan.9999          \033[1;36m║
+\033[1;36m║\033[38;5;204m▶ \033[38;5;204mZalo : \033[38;5;1204m0915.948.201                          \033[1;36m║
+\033[1;36m║\033[38;5;204m▶ \033[38;5;204mTool Do PhuocAn Code 😆                      \033[1;36m║
+\033[1;36m║\033[38;5;204m▶ \033[38;5;204mAnh Nhắc Em Nha , Đừng Bug 😭                \033[1;36m║
+\033[1;36m║\033[38;5;204m▶ \033[38;5;204mPhiên Bản Tool : 5.0 ( VIP )                 \033[1;36m║
+\033[1;36m╚═══════════════════════════════════════════════╝
+"""
+for X in banner:
+  sys.stdout.write(X)
+  sys.stdout.flush() 
+  sleep(0.001)
+
+init(autoreset=True)
+console = Console()
+
+os.system('cls' if os.name == 'nt' else 'clear')
+for i in range(1, 101):
+  sys.stdout.write(f"\r{BOLD} \033[38;5;155mĐANG LOAD MENU : [{i}% {'█' * (i // 2)}]{RESET}")
+  sys.stdout.flush()
+  sleep(0.003)  # Điều chỉnh thời gian chờ nếu cần
+sleep(1)
+os.system('cls' if os.name == 'nt' else 'clear')
+print(banner)
+thanhngang(65)
+
+print (Colorate.Diagonal(Colors.white_to_green, "╔══════════════════════╗         "))
+print (Colorate.Diagonal(Colors.white_to_green,"║  Tool Golike         ║          "))
+print (Colorate.Diagonal(Colors.white_to_green, "╚══════════════════════╝           "))
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m1\033[1;39m]\033[1;32m Tool Golike TikTok \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m ADB + Auto Click VIP \033[1;39m]")
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m2\033[1;39m]\033[1;32m Tool Golike SnapChat \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m ADB + Auto Click VIP \033[1;39m]")
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m3\033[1;39m]\033[1;32m Tool Golike Twitter ( X ) \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m Cookie VIP \033[1;39m]")
+print (Colorate.Diagonal(Colors.white_to_green, "┌───────────────────┐"))
+print (Colorate.Diagonal(Colors.white_to_green, "║ TraoDoiSub.com    ║"))
+print (Colorate.Diagonal(Colors.white_to_green, "└───────────────────┘"))
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m4\033[1;39m]\033[1;32m Tool Traodoisub TikTok \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m Auto Click VIP \033[1;39m]")
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m5\033[1;39m]\033[1;32m Tool Traodoisub Facebook \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m Cookie VIP \033[1;39m]")
+print (Colorate.Diagonal(Colors.white_to_green, "┌───────────────────┐"))
+print (Colorate.Diagonal(Colors.white_to_green, "║ Tuongtaccheo.com  ║"))
+print (Colorate.Diagonal(Colors.white_to_green, "└───────────────────┘"))
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m6\033[1;39m]\033[1;32m Tool Tuongtaccheo Facebook \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m Cookie VIP \033[1;39m]")
+print (Colorate.Diagonal(Colors.white_to_green, "┌───────────────────┐"))
+print (Colorate.Diagonal(Colors.white_to_green, "║      Pro5 Fb      ║"))
+print (Colorate.Diagonal(Colors.white_to_green, "└───────────────────┘"))
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m7\033[1;39m]\033[1;32m Tool Reg Page Pro5 Facebook \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m VIP \033[1;39m]")
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m8\033[1;39m]\033[1;32m Tool Buff Story Pro5 Facebook \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m VIP \033[1;39m]")
+print (Colorate.Diagonal(Colors.white_to_green, "┌───────────────────┐"))
+print (Colorate.Diagonal(Colors.white_to_green, "║      Tiện Ích     ║"))
+print (Colorate.Diagonal(Colors.white_to_green, "└───────────────────┘"))
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m9\033[1;39m]\033[1;32m Tool Spam SMS + Call \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m VIP \033[1;39m]")
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m10\033[1;39m]\033[1;32m Tool Tài Xỉu MD5 \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m Random \033[1;39m]")
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m11\033[1;39m]\033[1;32m Tool Vua Thoát Hiểm Xworld \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m Random \033[1;39m]")
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m0\033[1;39m]\033[1;32m Thoát Tool Gộp \033[1;39m[\033[38;5;204m Out \033[1;39m]")
+print (Colorate.Diagonal(Colors.white_to_green, "┌───────────────────┐"))
+print (Colorate.Diagonal(Colors.white_to_green, "║      Buff Vip     ║"))
+print (Colorate.Diagonal(Colors.white_to_green, "└───────────────────┘"))
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m12\033[1;39m]\033[1;32m Tool Buff Follow + Like API Free \033[1;39m[\033[38;5;204m FREE \033[1;39m]")
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m13\033[1;39m]\033[1;32m Tool Buff Like Free Fire \033[1;39m[\033[1;34m VIP \033[1;39m]")
+print (Colorate.Diagonal(Colors.white_to_green, "┌───────────────────┐"))
+print (Colorate.Diagonal(Colors.white_to_green, "║    Proxy + Mail   ║"))
+print (Colorate.Diagonal(Colors.white_to_green, "└───────────────────┘"))
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m14\033[1;39m]\033[1;32m Tool Đào Proxy \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m VIP \033[1;39m]")
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m15\033[1;39m]\033[1;32m Tool Lọc Proxy Live/Die \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m VIP \033[1;39m]")
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m16\033[1;39m]\033[1;32m Tool Đào Mail \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m VIP \033[1;39m]")
+print (Colorate.Diagonal(Colors.white_to_green, "┌───────────────────┐"))
+print (Colorate.Diagonal(Colors.white_to_green, "║    Enc + Decode   ║"))
+print (Colorate.Diagonal(Colors.white_to_green, "└───────────────────┘"))
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m17\033[1;39m]\033[1;32m Tool Enc Marshal \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m VIP \033[1;39m]")
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m18\033[1;39m]\033[1;32m Tool Enc Icon \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m VIP \033[1;39m]")
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m19\033[1;39m]\033[1;32m Tool DEC Marshal/PYC \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m VIP \033[1;39m]")
+print (Colorate.Diagonal(Colors.white_to_green, "┌───────────────────┐"))
+print (Colorate.Diagonal(Colors.white_to_green, "║       Tool PC     ║"))
+print (Colorate.Diagonal(Colors.white_to_green, "└───────────────────┘"))
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m20\033[1;39m]\033[1;32m Tool Zefoy No Auto Captcha ( TỰ GIẢI CAPTCHA ) \033[1;39m[\033[32;5;245m\033[1m\033[38;5;39m PC VIP \033[1;39m]")
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m21\033[1;39m]\033[1;32m Tool Nuôi Acc Tiktok Clone \033[1;39m[\033[38;5;204m BETA + DỄ LỖI \033[1;39m]")
+print(                    f"\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[1;32mNhập \033[1;39m[\033[1;35m22\033[1;39m]\033[1;32m Tool Golike FB Đa Luồng \033[1;39m[\033[38;5;204m BETA + DỄ LỖI \033[1;39m]")
+
+thanhngang(65)
+
+chon = input("\033[1;39m[\033[1;36m•_•\033[1;39m] => \033[32;5;245m\033[1m\033[38;5;39mNhập \033[1;33mSố \033[1;34mTool \033[38;5;204mMà \033[38;5;155mBạn \033[1;35mCần \033[1;36mChạy : \033[38;5;204m")  
+
+os.system('cls' if os.name == 'nt' else 'clear')
+for i in range(1, 101):
+  sys.stdout.write(f"\r{BOLD} \033[38;5;204mCHỜ ĐỢI LÀ HẠNH PHÚC: [{i}% {'█' * (i // 2)}]{RESET}")
+  sys.stdout.flush()
+  sleep(0.03)  # Điều chỉnh thời gian chờ nếu cần
+  
+
+
+# hàm chống bug mạng tránh mấy a bug lỏd 🤟
+def check_internet_connection():
+    try:
+        response = requests.get("https://google.com/", timeout=5)
+        return True
+    except requests.ConnectionError:
+        return False
+if not check_internet_connection():
+    print("\033[1;31mCheck Mạng Wifi Hoặc 4G! ")
+    sleep(0.5)
+    exit()
+
+if chon == "1":
+    exec(requests.get('https://raw.githubusercontent.com/Phuocnifepkay/ggggg/refs/heads/main/ggfghuhu66643eszfugfuu.py').text)
+if chon == "2":
+     exec(requests.get('https://raw.githubusercontent.com/Phuocnifepkay/ggggg/refs/heads/main/sexgay.py').text)
+if chon == "3":
+	exec(requests.get('https://raw.githubusercontent.com/Phuocnifepkay/uhgcc/refs/heads/main/Ggttttttttttttttrtttfcvyu88ibb.py').text)
+if chon == "4":
+	exec(requests.get('https://raw.githubusercontent.com/Phuocnifepkay/l-n/refs/heads/main/tdstikv2.py').text)
+if chon == "5":
+	exec(requests.get('https://raw.githubusercontent.com/Cacdume-wq/cac/refs/heads/main/vvfyjvyhvvyujbr56ujn.py').text)
+if chon == "6":
+	exec(requests.get('https://raw.githubusercontent.com/Cacdume-wq/cac/refs/heads/main/kkk.py').text)
+if chon == "0":                                 
+# Chỉ thoát tool khi nhập số "0"
+	exec(requests.get('https://raw.githubusercontent.com/Khanh23047/thoattool/main/.github/workflows/main.yml').text)
+if chon == "7":
+	exec(requests.get('https://raw.githubusercontent.com/Cacdume-wq/bomaykjjj/refs/heads/main/%C6%B0kwkkwkwksksskkzzjjzjzjxkxkzkzizkxkxkx.py').text)
+if chon == "8":
+    exec(requests.get('https://raw.githubusercontent.com/Phuocnifepkay/l-n/refs/heads/main/strpro5.py').text)
+if chon == "9":
+	exec(requests.get('https://raw.githubusercontent.com/Cacdume-wq/cac/refs/heads/main/sdftgvvvvfffccr4444ty66y6.py').text)
+if chon == "10":
+	exec(requests.get('https://raw.githubusercontent.com/Phuocnifepkay/l-n/refs/heads/main/Taixiu.py').text)
+if chon == "11":
+	exec(requests.get('https://raw.githubusercontent.com/Cacdume-wq/cac/refs/heads/main/sex.py').text)
+if chon == "12":
+	exec(requests.get('https://raw.githubusercontent.com/Cacdume-wq/cac/refs/heads/main/cd%20/sdcard/pkkkk.py').text)
+if chon == "13":
+	exec(requests.get('https://raw.githubusercontent.com/Cacdume-wq/bomaykjjj/refs/heads/main/kkkkkkk.py').text)
+if chon == "14":
+	exec(requests.get('https://raw.githubusercontent.com/Cacdume-wq/bomaykjjj/refs/heads/main/concaccccc.py').text)
+if chon == "15":
+	print("Vô tool 14 là có à")
+if chon == "16":
+	exec(requests.get('https://raw.githubusercontent.com/Cacdume-wq/cac/refs/heads/main/vggg555555.py').text)
+if chon == "17":
+	exec(requests.get('https://raw.githubusercontent.com/Cacdume-wq/cac/refs/heads/main/cc-cccc76655thv%40%2C*%40.py').text)
+if chon == "18":
+	exec(requests.get('https://raw.githubusercontent.com/Cacdume-wq/cac/refs/heads/main/cc-cccc76655thv%40%2C*%40.py').text)
+if chon == "19":
+	exec(requests.get('https://raw.githubusercontent.com/KhanhNguyen9872/kramer-specter_deobf/main/kramer-specter-deobf.py').text)
+if chon == "20":
+	exec(requests.get('https://raw.githubusercontent.com/Cacdume-wq/bomaykjjj/refs/heads/main/zeij77yvvj877foy788yhbjii.py').text)
+if chon == "21":
+	exec(requests.get('https://raw.githubusercontent.com/Cacdume-wq/bomaykjjj/refs/heads/main/Skibiskigf67i8yy899999.py').text)
+if chon == "22":
+	exec(requests.get('https://raw.githubusercontent.com/Phuocnifepkay/uhgcc/refs/heads/main/golikefbdaluong.py').text)
+# if chon == 
+
+    
